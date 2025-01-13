10a8e72767c2813e122ecfa4ceae6e8d43457343
Margaridamx <margaridamx@gmail.com>
2025-01-13 14:51:51 +0000

    comment out

--- src/app/back/page.tsx
+++ src/app/back/page.tsx
@@ -1,18 +1,20 @@
-import EditGreeting from "../components/editGreeting"
 //since we are using TypeScript we need to declare our type first
 type Greeting = {
   greeting:string;
   _id: string;
 }
 export default async function Back() {
-  const baseUrl = "https://m5g2-back-end-no-sql.vercel.app"
-  const response = await fetch(`${baseUrl}/api`)
-  const greetings: Greeting[] = await response.json()
-  return (
-    <div>
-      {greetings.map(greetingObj=>
-        <EditGreeting greetingObj={greetingObj} key={greetingObj._id.toString()}/>
-      )}
-    </div>
-  )
+  return <></>
+  // const baseUrl = "http://localhost:3000"
+  // const response = await fetch(`${baseUrl}/api`)
+  // const greetings: Greeting[] = await response.json()
+  // return (
+  //   <div>
+  //     {greetings.map(greetingObj=>
+  //       <h1 key={greetingObj._id.toString()}>
+  //         {greetingObj.greeting}
+  //       </h1>
+  //     )}
+  //   </div>
+  // )
 }
