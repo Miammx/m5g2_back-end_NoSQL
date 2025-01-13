4af2939532b14d2a777ef9f53fb880821d32d055
Margaridamx <margaridamx@gmail.com>
2025-01-13 14:55:42 +0000

    comment in

--- src/app/back/page.tsx
+++ src/app/back/page.tsx
@@ -4,17 +4,16 @@ type Greeting = {
   _id: string;
 }
 export default async function Back() {
-  return <></>
-  // const baseUrl = "http://localhost:3000"
-  // const response = await fetch(`${baseUrl}/api`)
-  // const greetings: Greeting[] = await response.json()
-  // return (
-  //   <div>
-  //     {greetings.map(greetingObj=>
-  //       <h1 key={greetingObj._id.toString()}>
-  //         {greetingObj.greeting}
-  //       </h1>
-  //     )}
-  //   </div>
-  // )
+  const baseUrl = "http://localhost:3000"
+  const response = await fetch(`${baseUrl}/api`)
+  const greetings: Greeting[] = await response.json()
+  return (
+    <div>
+      {greetings.map(greetingObj=>
+        <h1 key={greetingObj._id.toString()}>
+          {greetingObj.greeting}
+        </h1>
+      )}
+    </div>
+  )
 }
