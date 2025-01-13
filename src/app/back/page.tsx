57bff76c65a8cf405c8c3e7672b67983c3b83e49
Margaridamx <margaridamx@gmail.com>
2025-01-13 14:32:09 +0000

    first commit

--- src/app/back/page.tsx
+++ src/app/back/page.tsx
@@ -1,4 +1,18 @@
-export default function Back() {
-    console.log("hello back end")
-    return (<h1>Some Server Side rendered content</h1>)
-  }
+import EditGreeting from "../components/editGreeting"
+//since we are using TypeScript we need to declare our type first
+type Greeting = {
+  greeting:string;
+  _id: string;
+}
+export default async function Back() {
+  const baseUrl = "http://localhost:3000"
+  const response = await fetch(`${baseUrl}/api`)
+  const greetings: Greeting[] = await response.json()
+  return (
+    <div>
+      {greetings.map(greetingObj=>
+        <EditGreeting greetingObj={greetingObj} key={greetingObj._id.toString()}/>
+      )}
+    </div>
+  )
+}
